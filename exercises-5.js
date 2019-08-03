efdc1e913b867a6790d78200baa2f5d000976083
arif05rachman <arif05rachman@gmail.com>
2019-08-04 01:40:19 +0700

    exercises-5

--- exercises-5.js
+++ exercises-5.js
@@ -4,7 +4,6 @@ function palindrome(kata) {
     var palindrome = '';
     for(i=kata.length-1; i>=0; i--) {
         palindrome+=kata[i];
-        console.log(kata[i])
     }
     return kata === palindrome;
   }
