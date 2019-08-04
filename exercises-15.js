be08f18d963ac9ed4c39b10061c0c3b8bab90236
arif05rachman <arif05rachman@gmail.com>
2019-08-05 00:17:58 +0700

    add exercises-15.js

---
+++ exercises-15.js
@@ -0,0 +1,24 @@
+function groupAnimals(animals) {
+    animals.sort();
+    var arrAnimal = [];
+
+    var hurufAwal = animals[0][0];
+    var arrayKelompok = []
+    for (var i=0; i<animals.length; i++) {
+        if(hurufAwal==animals[i][0]) {
+            arrayKelompok.push(animals[i]);
+        }else {
+            arrAnimal.push(arrayKelompok);
+            arrayKelompok = [];
+            arrayKelompok.push(animals[i]);
+            hurufAwal = animals[i][0]; 
+        }
+    }
+    arrAnimal.push(arrayKelompok);
+    return arrAnimal;
+  
+  // TEST CASES
+  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+  // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+  // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
